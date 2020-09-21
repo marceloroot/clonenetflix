47953e94663f24c9aa0d70f6e2424d75311cb640
Marcelo <marcelo.lima.gomes.23@gmail.com>
2020-09-21 17:11:53 -0300

    primeiro colonen

---
+++ src/components/MoviewRow.js
@@ -0,0 +1,33 @@
+import React,{useState} from 'react';
+import './MoviewRow.css';
+
+export default ({title,items}) => {
+   
+    let mapImg;
+  console.log(items);
+     if (items){
+        mapImg = items.results.map(item =>{
+            return (
+             <div key={item.id} className="moveRow--item">
+               <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}  />
+              </div>
+            )
+        })
+
+     }
+
+
+
+    
+
+    return (
+        <div className="movieRow">
+           <h2>{title}</h2>
+           <div className="movieRow--listarea">
+               <div className="movieRow--list">
+                   {mapImg}
+               </div>
+           </div>
+        </div>
+    )
+}
